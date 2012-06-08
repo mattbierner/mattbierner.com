ccecbc16b2574d827b4193b9380e6ac1f9eb1da6
Matt Bierner <mattbierner@gmail.com>
2012-06-08 00:34:39 -0700

    Basic search support with Haystack.

--- mattbierner_com/pages/search_indexes.py
+++ mattbierner_com/pages/search_indexes.py
@@ -7,12 +7,23 @@ from mattbierner_com.pages.models import Page
 
 class PageIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    
     created = indexes.DateField(model_attr='created')
     tags = indexes.MultiValueField() 
+    brief = indexes.CharField(model_attr='brief')
+
+    header = indexes.CharField()
+    body = indexes.CharField()
 
     class Meta:
         model = Page
 
+    def prepare_header(self, obj): 
+        return obj.header.rendered 
+    
+    def prepare_body(self, obj): 
+        return obj.body.rendered 
+    
     def prepare_tags(self, obj): 
         return [tag.name for tag in obj.tags.all()] 
 
--- mattbierner_com/views.py
+++ mattbierner_com/views.py
@@ -1,6 +1,10 @@
+from collections import defaultdict
+
 from django.views.generic.base import View, TemplateResponseMixin
 from django.shortcuts import get_object_or_404
 from django.http import Http404
+
+from haystack.query import SearchQuerySet
 
 from taggit.models import Tag
 
@@ -19,9 +23,15 @@ class IndexView(View, TemplateResponseMixin):
         if query is None:
             pages = Page.objects.all()
         else:
-            pages = None
+            pages = [x.object for x in SearchQuerySet().filter(content=query)]
         
+        # build a counted dict of tags
+        tags = defaultdict(int)
+        for page in pages:
+            for tag in page.tags.all():
+                tags[tag] += 1
+
         return {
-            'tags': Tag.objects.all(),
+            'tags': [x[0] for x in sorted(tags.items(), reverse=True, key=lambda x: x[1])],
             'pages': pages,
         }
