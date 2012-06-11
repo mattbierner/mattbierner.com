3f4ded80896b5aecb1135d637d0eee3c7218961f
Matt Bierner <mattbierner@gmail.com>
2012-06-10 17:44:09 -0700

    History working correctly, restyling.

--- mattbierner_com/static/script/main.js
+++ mattbierner_com/static/script/main.js
@@ -1,35 +1,62 @@
 $(function()
 {
-    var page = undefined;
+    var root_page = {
+        title: $('#page_header h1').text(),
+        body: $('article').html(),
+    };
     
-    // Setup
+    
+    var setupPage = function(p)
+    {
+        $('article').html(p['body']);
+        $('#page_header h1').text(p['title']);
+    };
+    
+    // Handle search
+    $("#root_search from").bind("onsubmit", function(e)
+    {
+        
+    });
+    
+    // Handle clicks on SearchResult
     $("#search_results .SearchResultHeader a").live("click", function(e)
     {
         e.preventDefault(); // cancel default link action
         
-        // If already at url, noop 
+        // if already at url, noop 
         var dest = $(this).attr('href');
         if (window.location.pathname == dest)
             return;
-        
-        // Push the state.
-        var title = $(this).text();
-
-        history.pushState({
-            page: page
-        }, title,  dest);
-        
-        // Set new page title.
-        $('.PageHeader h1').text(title);
-        
-        // request new page content
-        $.getJSON('/page?url=' + encodeURI(dest), function(page)
-        {
-            if (!page)
-                return;
             
-            $('.PageBody').html(page['body']);
+        // set new page title.
+        var title = $(this).text();
+        $('#page_header h1').text(title);
+        
+        // Request new page content
+        $.getJSON('/page?url=' + encodeURI(dest), function(p)
+        {
+            if (!p)
+                return;
+                
+            // push the state.
+            history.pushState({
+                page: p
+            }, p['title'],  dest);
+            
+            // setup new page
+            setupPage(p);    
         });
     });
     
+    
+    // Handle back navigation
+    window.onpopstate = function(e)
+    {
+        var state = e.state;
+        if (state)
+            setupPage(state['page']);
+        else
+            setupPage(root_page);
+        
+    };
 });
