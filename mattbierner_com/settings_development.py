77b6f9e7d4c2601379c5bdb27187bb4b1c718586
Matt Bierner <mattbierner@gmail.com>
2012-06-13 21:33:07 -0700

    Updating settings.

--- mattbierner_com/settings_development.py
+++ mattbierner_com/settings_development.py
@@ -13,6 +13,14 @@ DATABASES = {
     }
 }
 
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+}
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
--- mattbierner_com/settings_production.py
+++ mattbierner_com/settings_production.py
@@ -1,8 +1,33 @@
 from settings_base import *
 
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql.mattbierner_com.cnf ',
+        },
+    }
+}
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+}
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+
+
+STATIC_ROOT = '/www/mattbierner_com/static'
